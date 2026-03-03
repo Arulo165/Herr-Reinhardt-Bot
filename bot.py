ca8fbec30022bd67bc8948c594f4ce3b17b658cd
Arulo <arulomodding@gmail.com>
2026-03-03 20:44:36 +0000

    Update target posting time in bot.py
    Change the target posting time from 20:50 to 20:48.

--- bot.py
+++ bot.py
@@ -98,7 +98,7 @@ async def daily_news():
     while not bot.is_closed():
         now = datetime.now()
         # Jeden Tag um 9:00 Uhr posten
-        target = datetime.combine(now.date(), time(20, 50))
+        target = datetime.combine(now.date(), time(20, 48))
         if now >= target:
             from datetime import timedelta
             target += timedelta(days=1)
